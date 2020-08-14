42d2f26b81831aacfea8f52e80d6ac3f070f491d
Ruan Moreira <ruaanmoreira2508@gmail.com>
2020-08-14 11:51:31 -0300

    primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,11 @@
 package application;
 
-public class Program {
+import boardgame.Position;
 
+public class Program {
+	public static void main(String[] args) {
+	Position pteste = new Position(3, 4);
+	System.out.println(pteste);
+			
+	}	
 }
